b00eb5de5fb02a7c588731781c2883c8e80af681
shadow <961237327@qq.com>
2020-10-22 13:15:40 +0800

    第四次修改student上传

--- src/com/shadow/entity/Student.java
+++ src/com/shadow/entity/Student.java
@@ -3,6 +3,7 @@ package com.shadow.entity;
 public class Student {
     private String id;
     private Integer age;
+    private String address;
 
     public String getId() {
         return id;
@@ -18,5 +19,13 @@ public class Student {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
