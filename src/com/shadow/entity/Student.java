1c27608cb458fbaac477dcc00bb4e3cb4f95b615
shadow <961237327@qq.com>
2020-10-22 13:12:40 +0800

    第三次修改Student类

--- src/com/shadow/entity/Student.java
+++ src/com/shadow/entity/Student.java
@@ -3,4 +3,20 @@ package com.shadow.entity;
 public class Student {
     private String id;
     private Integer age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
